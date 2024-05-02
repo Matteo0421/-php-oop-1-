d772eac7e0529017006013353f3a3cb4c6948108
Matteo0421 <matteo.carola99@gmail.com>
2024-05-02 14:27:20 +0200

    [end]first-milestone

--- Model/Movie.php
+++ Model/Movie.php
@@ -4,12 +4,19 @@ class Movie
   public $title;
   public $year;
   public $director;
+  public $protagonist;
   
 
-  public function __construct(string $_title, string $_year, string $_director)
+  public function __construct(string $_title, string $_year, string $_director, string $_protagonist)
   {
     $this->title = $_title;
     $this->year = $_year;
     $this->director = $_director;
+    $this->protagonist = $_protagonist;
+  }
+
+  public function getTitleHero()
+  {
+    echo "$this->title, il protagonista è $this->protagonist";
   }
 }
--- index.php
+++ index.php
@@ -1,11 +1,38 @@
 <?php 
 require_once __DIR__ . '/Model/Movie.php';
 
-$avatar = new Movie ('Avatar il film', '2010', 'James Cameron');
+$avatar = new Movie ('Avatar il film', '2010', 'James Cameron', 'Sam Worthington' );
 
-$rocky = new Movie ('Rocky Balboa', '2006', 'Sylvester Stallone');
+$rocky = new Movie ('Rocky Balboa', '2006', 'Sylvester Stallone', 'Sylvester Stallone');
 
 var_dump($avatar);
 
 var_dump($rocky)
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>First OOP MOVIE</title>
+</head>
+<body>
+  <h1>I MIEI FILM PREFERITI</h1>
+
+  <h2>Avatar</h2>
+  <p>Titolo: <?php echo $avatar->title; ?></p>
+  <p>Anno: <?php echo $avatar->year; ?></p>
+  <p>Regista: <?php echo $avatar->director; ?></p>
+  <p>Protagonista: <?php echo $avatar->protagonist; ?></p>
+  <h4><?php $avatar->getTitleHero() ?></h4>
+
+  <h2>Rocky Balboa</h2>
+  <p>Titolo: <?php echo $rocky->title; ?></p>
+  <p>Anno: <?php echo $rocky->year; ?></p>
+  <p>Regista: <?php echo $rocky->director; ?></p>
+  <p>Protagonista: <?php echo $rocky->protagonist; ?></p>
+  <h4><?php $rocky->getTitleHero() ?></h4>
+
+</body>
+</html>
