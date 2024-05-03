010ffb53d76690876e70b847b52d0f938012d352
Matteo0421 <matteo.carola99@gmail.com>
2024-05-03 10:01:28 +0200

    [add]db

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <?php 
 require_once __DIR__ . '/Model/Movie.php';
 
-$avatar = new Movie ('Avatar il film', '2010', 'James Cameron', 'Sam Worthington' );
 
-$rocky = new Movie ('Rocky Balboa', '2006', 'Sylvester Stallone', 'Sylvester Stallone');
-
-var_dump($avatar);
-
-var_dump($rocky)
+$movies = [
+  new Movie('Avatar il film', '2010', 'James Cameron', 'Sam Worthington'),
+  new Movie('Rocky Balboa', '2006', 'Sylvester Stallone', 'Sylvester Stallone')
+];
 ?>
 
 <!DOCTYPE html>
@@ -16,23 +14,30 @@ var_dump($rocky)
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>First OOP MOVIE</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
-  <h1>I MIEI FILM PREFERITI</h1>
+  <h1 class="text-center mb-5" >I MIEI FILM PREFERITI</h1>
 
-  <h2>Avatar</h2>
-  <p>Titolo: <?php echo $avatar->title; ?></p>
-  <p>Anno: <?php echo $avatar->year; ?></p>
-  <p>Regista: <?php echo $avatar->director; ?></p>
-  <p>Protagonista: <?php echo $avatar->protagonist; ?></p>
-  <h4><?php $avatar->getTitleHero() ?></h4>
-
-  <h2>Rocky Balboa</h2>
-  <p>Titolo: <?php echo $rocky->title; ?></p>
-  <p>Anno: <?php echo $rocky->year; ?></p>
-  <p>Regista: <?php echo $rocky->director; ?></p>
-  <p>Protagonista: <?php echo $rocky->protagonist; ?></p>
-  <h4><?php $rocky->getTitleHero() ?></h4>
+  <div class="row">
+      <?php foreach ($movies as $movie): ?>
+        <div class="col-md-6">
+          <div class="card mt-3">
+            <div class="card-body">
+              <h5 class="card-title"><?php echo $movie->title; ?></h5>
+              <p class="card-text">Titolo: <?php echo $movie->title; ?></p>
+              <p class="card-text">Anno: <?php echo $movie->year; ?></p>
+              <p class="card-text">Regista: <?php echo $movie->director; ?></p>
+              <p class="card-text">Protagonista: <?php echo $movie->protagonist; ?></p>
+              <h6 class="card-subtitle mb-2 text-muted"><?php $movie->getTitleHero() ?></h6>
+            </div>
+          </div>
+        </div>
+      <?php endforeach; ?>
+    </div>
 
 </body>
 </html>
